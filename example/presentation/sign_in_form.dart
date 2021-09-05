17c048bd2715bfbcb41fd97d02ea4d50e05fa077
Alp Sahin <alp@sahin.club>
2021-09-05 15:43:30 +0300

    Added formkey on example app

--- example/presentation/sign_in_form.dart
+++ example/presentation/sign_in_form.dart
@@ -5,7 +5,7 @@ import 'package:form_handling/form_handling.dart';
 import '../application/sign_in_notifier.dart';
 import '../domain/sign_in_failure.dart';
 import 'custom_text_field.dart';
-
+ 
 typedef SignInFormState = CustomFormState<SignInFormFields, SignInFailure>;
 
 final signInNotifierProvider = StateNotifierProvider<SignInNotifier, SignInFormState>((ref) => SignInNotifier());
@@ -16,33 +16,37 @@ class SignInForm extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final state = ref.watch(signInNotifierProvider);
+    final notifier = ref.watch(signInNotifierProvider.notifier);
     return Scaffold(
       appBar: AppBar(
         title: Text('Sign In'),
       ),
       extendBody: true,
-      body: SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 40),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: [
-              CustomTextField.email(
-                fieldObject: state.fields.email,
-              ),
-              const SizedBox(height: 20,),
-              CustomTextField.password(
-                fieldObject: state.fields.password,
-              ),
-              const SizedBox(
-                height: 20,
-              ),
-              ElevatedButton(
-                onPressed: ref.read(signInNotifierProvider.notifier).signIn,
-                child: Text('Sign In'),
-              ),
-            ],
+      body: Form(
+        key: notifier.formKey,
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 40),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                CustomTextField.email(
+                  fieldObject: state.fields.email,
+                ),
+                const SizedBox(height: 20,),
+                CustomTextField.password(
+                  fieldObject: state.fields.password,
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                ElevatedButton(
+                  onPressed: notifier.signIn,
+                  child: Text('Sign In'),
+                ),
+              ],
+            ),
           ),
         ),
       ),
